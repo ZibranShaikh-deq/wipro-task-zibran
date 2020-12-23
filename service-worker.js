6c6b790c0895653dca6b65a863ebca2246aa1b9a
Zibran_Shaikh <zshaikh@deqode.com>
2020-12-23 19:18:17 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "wipro-task-zibran/precache-manifest.925d2ae89f2922512d155cacecedb1f0.js"
+  "/wipro-task-zibran/precache-manifest.49727f7896d663814ef4bd79411e85a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("wipro-task-zibran/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wipro-task-zibran/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
