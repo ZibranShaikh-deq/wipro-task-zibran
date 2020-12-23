4620669bbc3ff7e0734d8adcdb05cba2f17db302
Zibran_Shaikh <zshaikh@deqode.com>
2020-12-23 18:45:47 +0530

    Updates

---
+++ precache-manifest.d4a0d9aea960bb3ad0b30830a5b5a854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f18c739b3a996ebacd6dbcbade66da",
+    "url": "/wipro-task-zibran/index.html"
+  },
+  {
+    "revision": "1d0dd0ff11c8994d8cf9",
+    "url": "/wipro-task-zibran/static/css/main.491886b5.chunk.css"
+  },
+  {
+    "revision": "52d79a8e221aa5f34377",
+    "url": "/wipro-task-zibran/static/js/2.4ab0ed05.chunk.js"
+  },
+  {
+    "revision": "1d0dd0ff11c8994d8cf9",
+    "url": "/wipro-task-zibran/static/js/main.bb2032c5.chunk.js"
+  },
+  {
+    "revision": "e4bf83a041e8429f6aa2",
+    "url": "/wipro-task-zibran/static/js/runtime~main.ba401be9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d97989ad6ca15c9e4186d3dd980050b8.js"
+  "/wipro-task-zibran/precache-manifest.d4a0d9aea960bb3ad0b30830a5b5a854.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wipro-task-zibran/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
